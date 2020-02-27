85ea388935726983b9219ae2ee4799ff889f2f7a
Nikhil C Patil <nikhil.patil1aim@gmail.com>
2020-02-27 19:30:31 +0530

    Added cors

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+    next()
+});
+
 app.use('/users', userRouter);
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/frontend/index.html');
